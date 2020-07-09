82225df37c9bfad405c8d52b4b22c86f671ca39f
yu.yi <novak12@163.com>
2020-07-09 23:55:07 +0800

    Add login page

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_demo/route/RouterTestRoute.dart';
 import 'package:flutter_demo/route/newroute.dart';
 import 'package:flutter_demo/widgets/MyHomePage.dart';
+import 'package:flutter_demo/widgets/login.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       ),
       //用注册表来注册路由
       routes: {
-        "/": (context) => MyHomePage(title: 'Flutter Demo Home Page'),
+        "/": (context) => Login(),
         "new_page": (context) => NewsRoute(),
         "test_route": (context) => RouterTestRoute()
       },
